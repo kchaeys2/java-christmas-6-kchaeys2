9813fa61c377fc72b62ee56daad71520212f234d
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-11-13 23:44:12 +0900

    feat (DiscountService) : 디저트 메뉴 ,메인 메뉴 할인 함수 추가

--- src/main/java/service/DiscountService.java
+++ src/main/java/service/DiscountService.java
@@ -1,8 +1,14 @@
 package service;
 
+import model.Category;
 import model.Date;
+import model.Orders;
 
 public class DiscountService {
+    private final Orders orders;
+    public DiscountService(Orders orders,Integer orderAmount){
+        this.orders = orders;
+    }
     public Boolean possibleDiscount(Integer orderAmount){
         return orderAmount > 10000;
     }
@@ -17,5 +23,17 @@ public class DiscountService {
     }
     public Boolean eventChampagne(Integer orderAmount){
         return orderAmount >= 120000;
+    }
+    public Integer discountWeek(Date date){
+        if (date.checkWeek().equals("평일")){
+            return Math.toIntExact(orders.countCategory(Category.DESSERT) * 2023);
+        }
+        return 0;
+    }
+    public Integer discountWeekend(Date date){
+        if(date.checkWeek().equals("주말")){
+            return Math.toIntExact(orders.countCategory(Category.MAIN) * 2023);
+        }
+        return 0;
     }
 }
